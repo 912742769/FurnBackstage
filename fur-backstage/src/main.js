7b3c6c4ec99b61e445959e219f9991678d3661a2
912742769 <912742769@qq.com>
2019-04-19 10:01:19 +0800

    引入axios依赖

--- fur-backstage/src/main.js
+++ fur-backstage/src/main.js
@@ -3,6 +3,43 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// ajax 请求库
+import axios from 'axios'
+Vue.prototype.$http=axios
+//统一设置请求前缀
+axios.defaults.baseURL='http://127.0.0.1:3000'
+
+/* //请求拦截
+axios.interceptors.request.use(config =>{
+  //每次发请求之前都需要判断token
+  //如果存在，则在请求头上带上 token，一起发送给后台验证
+  const token = localStorage.getItem('token')
+  token && (config.headers["Authorization"] = "Bearer "+token)
+  return config
+},error =>{
+return Promise.reject(error)
+})
+//响应拦截
+axios.interceptors.response.use(response =>{
+if(response.data.token){
+  localStorage.getItem('token',response.data.token)
+}
+return response
+},error =>{
+let res = error.response
+switch(res.status){
+  case 401://taken 验证失败，需要重新登陆
+    localStorage.removeItem('token')
+    return router.replace({
+      path:'/login'
+    }) 
+  case 404:
+    console.log('404错误')
+    break
+}
+return Promise.reject(error)
+}) */
+
 Vue.config.productionTip = false
 
 new Vue({
