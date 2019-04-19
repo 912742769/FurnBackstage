335f9d1403f040e72c1211078e8352b67c3e1827
912742769 <912742769@qq.com>
2019-04-19 10:03:56 +0800

    引入axios依赖

--- fur-backstage/src/main.js
+++ fur-backstage/src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 import axios from 'axios'
 Vue.prototype.$http=axios
 //统一设置请求前缀
-axios.defaults.baseURL='http://127.0.0.1:3000'
+axios.defaults.baseURL='http://127.0.0.1:3000' 
 
 /* //请求拦截
 axios.interceptors.request.use(config =>{
