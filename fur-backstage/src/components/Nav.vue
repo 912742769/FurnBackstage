b3deb20bb3bc1ef62b6e6646e0a577024d08e82f
757098151 <757098151@qq.com>
2019-04-22 14:01:24 +0800

    上传导航框

---
+++ fur-backstage/src/components/Nav.vue
@@ -0,0 +1,107 @@
+<template>
+
+    <el-container style="width:'100%' border: 1px solid #eee">
+        <el-aside width="200px">
+            <div id="icon"></div>
+            <el-menu :default-openeds="['1']">
+            <el-menu-item index="1">
+                <template slot="title"><i class="el-icon-menu"></i>概览</template>
+            </el-menu-item>
+            <el-submenu index="2">
+                <template slot="title"><i class="el-icon-goods"></i>商品管理</template>
+                <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                <el-menu-item index="2-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-submenu index="3">
+                <template slot="title"><i class="el-icon-tickets"></i>订单管理</template>
+                <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                <el-menu-item index="3-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="3-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-submenu index="4">
+                <template slot="title"><i class="el-icon-setting"></i>订单管理</template>
+                <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="4-1">选项1</el-menu-item>
+                <el-menu-item index="4-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                <el-menu-item index="4-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="4-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="4-4-1">选项4-1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            </el-menu>
+        </el-aside>
+        <Main />
+   </el-container>
+
+</template>
+
+<style>
+   .el-aside {
+    color: #333;
+  }
+  .el-dropdown-link {
+    cursor: pointer;
+
+  }
+  #icon{
+    width: 50px;
+    height: 50px;
+    /* background: url(assets/usericon.png) no-repeat center; */
+    background-size: cover;
+    margin: 0 auto;
+  }
+  .header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  #user-item-box{
+    line-height: 30px;
+  }
+  .usericon{
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    float: left;
+  }
+  .usericon img{
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+
+<script>
+import Main from '@/views/Main.vue'
+
+  export default {
+    name: 'home',
+    components: {
+      Main
+    }
+  };
+</script>
